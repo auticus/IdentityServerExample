b5b25fd608b7896d0f4b0093792acf7160a085d9
Christopher Nye <icedcrow@yahoo.com>
2021-06-15 11:06:25 -0700

    Added a Movies client project to consume the api data

---
+++ Movies.client/Clients/IMovieAPIService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Movies.client.Models;
+
+namespace Movies.client.Clients
+{
+    public interface IMovieAPIService
+    {
+        Task<IEnumerable<Movie>> GetMovies();
+        Task<Movie> GetMovie(string id);
+        Task<Movie> CreateMovie(Movie movie);
+        Task<Movie> UpdateMovie(Movie movie);
+        Task DeleteMovie(int id);
+    }
+}
---
+++ Movies.client/Clients/MovieAPIService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Movies.client.Models;
+
+namespace Movies.client.Clients
+{
+    public class MovieAPIService : IMovieAPIService
+    {
+        public async Task<IEnumerable<Movie>> GetMovies()
+        {
+            var movies = new List<Movie>();
+            movies.Add(new Movie
+            {
+                Id = 1,
+                Genre = "Comics",
+                Title = "Sammy Screams",
+                Rating = "5.6",
+                ImageUrl = "images/src",
+                ReleaseDate = DateTime.Now,
+                Owner = "auticus"
+            });
+
+            return await Task.FromResult(movies);
+        }
+
+        public Task<Movie> GetMovie(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Movie> CreateMovie(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Movie> UpdateMovie(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteMovie(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
